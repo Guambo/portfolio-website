dc174f86ff0a476b5a1f183c683c6f0438941165
NPVallejos <nvallejos123@gmail.com>
2020-12-24 14:39:15 -0500

    Converted project to React project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      { /* Header Component */ }
+      { /* About Component */ }
+      { /* Projects Component */ }
+      { /* Contact Component */ }
+    </div>
+  );
+}
+
+export default App;
