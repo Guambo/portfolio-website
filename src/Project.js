78265937a9281d41f4437be15c502e269eeffd7c
NPVallejos <nvallejos123@gmail.com>
2020-12-31 00:01:44 -0500

    Added random music generator project
    ProjectSection.js & ProjectSection.css:
    +Added title header to projectSection.js
    +Added 4th project to projectSection.js
    +Added media query to projectSection.css
      - After viewport reaches 1135px it will change the
        projectSection__content flex-direction to column
      - Also make each project take up 100% of the screen
    
    Project.js & Project.css:
    +Added inline conditional rendering to return statement inside Projects
    component
    +Each project will now render either an itchio embed or a custom
    project header
    +The custom project header includes an image, a title, and a link to the
    project (mimics itch embed styling)

--- src/Project.js
+++ src/Project.js
@@ -4,13 +4,30 @@ import './Project.css';
 function Project(props) {
     return (
         <div className="project" style={{backgroundColor: props.backgroundColor}}>
-            <iframe
-                src={props.src}
-                frameborder={props.frameborder}>
-                    <a href={props.itchLink}>Goo Runner by
-                    Guambo</a>
-            </iframe>
-            <img src={props.image} />
+            {props.itchLink !== "" &&
+                <iframe
+                    src={props.iframeSrc}
+                    frameborder="0">
+                        <a href={props.itchLink}></a>
+                </iframe>
+            }
+            {props.itchLink == "" && 
+                <div className="project__header">
+                    {props.image !== "" && 
+                        <img src={props.image} />
+                    }
+                    <div className="project__headerRight">
+                        {props.title !== "" &&
+                            <h2>{props.title}</h2>
+                        }
+                        {props.playStoreLink !== "" &&
+                            <a href={props.playStoreLink}>
+                                <img srcset="https://static.itch.io/images/store_badges/google.png 1x, https://static.itch.io/images/store_badges/google@2x.png 2x" src="https://static.itch.io/images/store_badges/google.png"></img>
+                            </a>
+                        }
+                    </div>
+                </div>
+            }
             {props.descriptionParagraph}
         </div>
     )
