f6b4cf64fdff76343bd52c4dc314d7f16d55c672
NPVallejos <nvallejos123@gmail.com>
2021-01-03 20:39:09 -0500

    Added EmailJS for email support on Contact form
    Contact.js:
    -Added 5 state variables that hold each input field value
    -Submitting the form triggers emailjs.send(...) function which sends an
    email containing all of the form information to my personal email

--- src/Contact.js
+++ src/Contact.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.css';
 import contactImage from './contactImage.jpg';
 
 function Contact() {
+    const [fname, updateFname] = useState("");
+    const [lname, updateLname] = useState("");
+    const [email, updateEmail] = useState("");
+    const [phone, updatePhone] = useState("");
+    const [feedback, updateMessage] = useState("");
+
+    const sendFeedback = (templateId, variables) => {
+        window.emailjs.send(
+            "service_01wy4s5", templateId, variables
+        )
+        .then(res => {
+            console.log("Email successfully sent!");
+        })
+        .catch(err => 
+            console.error("Failed to send email.")
+        );
+    }
+
+    const handleSubmit = () => {
+        const templateId = "template_iabq0mc";
+        const name = fname + " " + lname;
+        
+        // Check that all fields are filled
+        if (fname === "" || lname === "" || feedback === "" || phone === "" || email === "") {
+            console.log("All fields must be filled out");
+            return;
+        }
+
+        sendFeedback(templateId, {
+            from_name: name,
+            message: feedback,
+            phone_number: phone, 
+            reply_to: email,
+        });
+    }
+
     return (
         <div className="contact" id="contact">
             <div className="contact__backgroundImage"></div>
@@ -17,16 +53,25 @@ function Contact() {
                 </div>
                 <form className="contact__form__input">
                     <p>First Name</p>
-                    <input type="text" required></input>
+                    <input type="text" onChange={(e) => updateFname(e.target.value)} required></input>
                     <p>Last Name</p>
-                    <input type="text" required></input>
+                    <input type="text" onChange={(e) => updateLname(e.target.value)} required></input>
                     <p>Email</p>
-                    <input type="text" required></input>
+                    <input type="text" onChange={(e) => updateEmail(e.target.value)} required></input>
                     <p>Phone</p>
-                    <input type="text" required></input>
+                    <input type="text" onChange={(e) => updatePhone(e.target.value)} required></input>
                     <p>Message</p>
-                    <textarea name="Enter message..." rows="10" cols="22" placeholder="Enter your message" required></textarea>
-                    <button>Submit</button>
+                    <textarea 
+                        id="Message"
+                        name="Message" 
+                        rows="10" 
+                        cols="22" 
+                        placeholder="Enter your message" 
+                        required
+                        onChange={(e) => updateMessage(e.target.value)}
+                    >
+                    </textarea>
+                    <button onClick={handleSubmit}>Submit</button>
                 </form>
             </div>
         </div>
